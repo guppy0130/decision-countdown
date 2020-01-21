d1876a322e3c503a18d7375fe547fb8865c8a150
Nick Yang <guppy0130@users.noreply.github.com>
2020-01-20 17:38:31 -0800

    Working (#2)

--- cypress/integration/basics.js
+++ cypress/integration/basics.js
@@ -1,7 +1,104 @@
+let nanoid = '';
 describe('Landing Page', function() {
-    it('Should say hello there', function() {
+    it('Should have basic elements', function() {
         cy.visit('/');
+        // there should be a title
+        cy.contains('Decision Countdown').should('have.length', 1).and('be.visible');
+        // there should be one slider for the time
+        cy.get('.b-slider').should('have.length', 1).and('be.visible');
+        // there should be 4 ticks
+        cy.get('.b-slider-tick-label').should('have.length', 4).and('be.visible');
+        // there should be only one submit button
+        cy.get('.is-success').should('have.length', 1).and('be.visible');
+        // there should be two input fields
+        cy.get('input').should('have.length', 2).and('be.visible');
+        // there should be three headers: question, time, options
+        for (let word of ['Question', 'Time', 'Options']) {
+            cy.contains(word).should('have.length', 1).and('be.visible');
+        }
+    });
 
-        cy.contains('hello there');
+    it('Should accept input', function() {
+        // write to the question
+        cy.get('input').first().type('Is this a question?');
+        // set a time
+        cy.contains('20').click();
+        // add some options
+        cy.get('input').last().type('Yes');
+        cy.get('.mdi-plus').click();
+        // clicking this resets the field
+        cy.get(':nth-child(3) > .field > .control > .input').should('be.empty');
+        // the other way to add an option is via the enter key
+        cy.get(':nth-child(3) > .field > .control > .input').type('No {enter}');
+        // and again, the field should be empty
+        cy.get(':nth-child(3) > .field > .control > .input').should('be.empty');
+        // there should be two trash cans
+        cy.get('.mdi-delete').should('have.length', 2).and('be.visible');
+    });
+
+    it('Shows a link, vote, and results after clicking', function() {
+        // we should submit the form now
+        cy.get('.is-success').click();
+        // there should be a link
+        cy.contains('respond').should('have.length', 1).and('be.visible');
+        // there should be a copy button, so let's click it
+        cy.get('.mdi-content-copy').should('have.length', 1).and('be.visible').click();
+        // a snackbar should be visible
+        cy.get('.snackbar').should('have.length', 1).and('be.visible').contains('Copied to clipboard');
+        // and there should be a vote button
+        cy.contains('Vote').should('have.length', 1).and('be.visible').and('have.attr', 'href').and('match', /respond/);
+        // and a results button
+        cy.contains('Results').should('have.length', 1).and('be.visible').and('have.attr', 'href').and('match', /results/);
+        cy.get('.has-addons > .control > .input').invoke('val')
+            .then(url => {
+                nanoid = url.slice(-6);
+                cy.log(nanoid);
+            });
+    });
+});
+
+describe('Respond Page', function() {
+    it('Should have basic elements', function () {
+        cy.visit(`/respond/${nanoid}`);
+        // there should be a title
+        cy.contains('Decision Countdown').should('have.length', 1).and('be.visible');
+        // the question should be determined and disabled
+        cy.get('input').should('have.length', 1).and('be.visible').and('be.disabled').invoke('val')
+            .then(query => {
+                cy.wrap(query).should('eq', 'Is this a question?');
+            });
+        // there should be one slider for the time
+        cy.get('.b-slider').should('have.length', 1).and('be.visible');
+        // there should be 4 ticks
+        cy.get('.b-slider-tick-label').should('have.length', 4).and('be.visible');
+        // there should be two options
+        cy.get('button').should('have.length', 2).and('be.visible');
+        cy.contains('Yes').should('have.length', 1).and('be.visible');
+        cy.contains('No').should('have.length', 1).and('be.visible');
+    });
+
+    it('Options should be clickable', function() {
+        // we should click one
+        cy.contains('Yes').click();
+        cy.contains('Yes').should('be.disabled').and('has.class', 'is-success');
+        cy.get('button').should('have.length', 1).and('be.visible');
+    });
+});
+
+describe('Votes Page', function() {
+    it('Should have basic elements', function () {
+        cy.visit(`/results/${nanoid}`);
+        // there should be a title
+        cy.contains('Decision Countdown').should('have.length', 1).and('be.visible');
+        // the question should be determined and there should be a searchable header
+        cy.contains('Is this a question?');
+        cy.get('input').should('have.length', 2).and('be.visible');
+        // and there should be a table
+        cy.get('.b-table').should('have.length', 1).and('be.visible');
+        // Yes should have a value of 1
+        cy.get(':nth-child(1) > .has-text-right').invoke('text')
+            .then(val => {
+                cy.wrap(val.trim()).should('eq', '1');
+            });
     });
 });
--- index.js
+++ index.js
@@ -1,10 +1,94 @@
 const express = require('express');
+const nanoid = require('nanoid/non-secure/generate');
+const hbs = require('express-hbs');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+const data = {};
+const renderables = {
+    title: 'Decision Countdown - bc you SUCK at choosing'
+};
+const colors = ['#2a2b2a','#337357','#ba7ea5','#2b4162','#235789'];
+
+app.engine('hbs', hbs.express4({
+    partialsDir: `${__dirname}/views/partials`,
+    layoutsDir: `${__dirname}/views/layout`
+}));
+app.set('view engine', 'hbs');
+app.set('views', `${__dirname}/views`);
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
-    res.send('hello there');
+    // start a poll
+    let result = Object.assign(renderables, {
+        accent: colors[Math.floor(Math.random() * 5)],
+        time: 0,
+        question: JSON.stringify(''),
+        options: JSON.stringify([]),
+        url: ''
+    });
+    res.render('index', result);
+});
+
+app.post('/add', (req, res) => {
+    // post JSON data here to add to data
+    // validation
+    if (req.body.time < 0 || req.body.time > 30) {
+        return res.status(400).send('Must pick 0-30s');
+    }
+    if (req.body.question === '') {
+        return res.status(400).send('Question cannot be empty');
+    }
+    if (req.body.options.length === 0 || req.body.options.length > 20) {
+        return res.status(400).send('Number of options should be 1-20');
+    }
+    // add to data under nanoid()
+    const id = nanoid('1234567890abcdef', 6);
+    const optionContainer = {
+        options: {}
+    };
+    req.body.options.forEach(option => {
+        optionContainer['options'][option] = 0;
+    });
+    data[id] = Object.assign(req.body, optionContainer);
+    return res.status(200).send(`${req.protocol}://${req.get('host')}/respond/${id}`);
+});
+
+const stringify = (obj) => {
+    const returnable = {};
+    for (const [k, v] of Object.entries(obj)) {
+        returnable[k] = JSON.stringify(v);
+    }
+    return returnable;
+};
+
+app.get('/results/:code', (req, res) => {
+    res.render('votes', Object.assign(Object.assign(renderables, stringify(data[req.params.code])), {url: `${req.protocol}://${req.get('host')}/respond/${req.params.code}`}));
+});
+
+app.get('/respond/:code', (req, res) => {
+    // other people can respond (add votes to data)
+    // what if code doesn't exist?
+    if (!data[req.params.code]) {
+        return res.sendStatus(404);
+    }
+    let result = Object.assign(Object.assign(renderables, stringify(data[req.params.code])), {url: `${req.protocol}://${req.get('host')}/respond/${req.params.code}`});
+    res.render('respond', result);
+});
+
+app.post('/respond/:code', (req, res) => {
+    // other people can respond (add votes to data)
+    // what if code doesn't exist?
+    if (!data[req.params.code]) {
+        return res.status(404).send('Poll not found');
+    }
+    if (!(req.body.vote in data[req.params.code]['options'])) {
+        return res.status(409).send('Option not found');
+    }
+    data[req.params.code]['options'][req.body.vote] += 1;
+    return res.sendStatus(200);
 });
 
 app.listen(port, () => {
